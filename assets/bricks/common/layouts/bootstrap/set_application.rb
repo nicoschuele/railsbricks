292372561d8723e61beb7770d5275de99b4a2cca
Nico Schuele -mini <nicoschuele@gmail.com>
2014-01-05 18:08:12 +0100

    adds brick for "no authentication / bootstrap 3"

---
+++ assets/bricks/common/layouts/bootstrap/set_application.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+require ENV['HOME']+"/railsbricks/assets/common_scripts/app_name.rb"
+
+template_path = ENV['HOME']+"/railsbricks/assets/bricks/common/layouts/bootstrap/application.html.erb"
+file_path = "app/views/layouts/application.html.erb"
+given_name = ARGV.shift
+
+file_content = File.read(template_path)
+file_content = replace_with_app_name(app_name(given_name), file_content)
+
+File.open(file_path,"w") do |f|
+  f.puts file_content
+end
