d7fda2d4a12054c139db4334ba6c44627f6c58c2
Nico Schuele -mini <nicoschuele@gmail.com>
2014-01-07 12:10:35 +0100

    corrects type in simple/application_controller.rb

--- assets/user_model/simple/application_controller.rb
+++ assets/user_model/simple/application_controller.rb
@@ -22,6 +22,6 @@ class ApplicationController < ActionController::Base
   def user_signed_in?
     @current_user ? true : false    
   end
-  helper_method: user_signed_in?
+  helper_method :user_signed_in?
   
 end
