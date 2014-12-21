37c55da5bc8ab5df30a9ceb0b6bbdb42f937d0ae
Nico Schuele -mini <nicoschuele@gmail.com>
2014-12-21 15:12:05 +0100

    changes max length of username to 15 char

--- lib/railsbricks/assets/models/devise_username/user.rb
+++ lib/railsbricks/assets/models/devise_username/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   # :username
   validates :username, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /\A[a-zA-Z0-9]*\z/, on: :create, message: "can only contain letters and digits"
-  validates :username, length: { in: 4..10 }
+  validates :username, length: { in: 4..15 }
   # :email
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   
